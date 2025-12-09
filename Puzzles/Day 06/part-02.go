60dacbe2b7447cef626baf995b72b70a97195d22
sarvbesh <atsarveshyadav@gmail.com>
2025-12-09 13:01:10 +0530

    Day 6 done

---
+++ Puzzles/Day 06/part-02.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	grid := [][]rune{}
+	for scanner.Scan() {
+		grid = append(grid, []rune(scanner.Text()))
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	rows := len(grid)
+	cols := len(grid[0])
+
+	isBlankCol := func(c int) bool {
+		for r := 0; r < rows; r++ {
+			if grid[r][c] != ' ' {
+				return false
+			}
+		}
+		return true
+	}
+
+	var total int64 = 0
+	c := 0
+
+	for c < cols {
+		if isBlankCol(c) {
+			c++
+			continue
+		}
+
+		start := c
+		for c < cols && !isBlankCol(c) {
+			c++
+		}
+		end := c
+
+		op := strings.TrimSpace(string(grid[rows-1][start:end]))
+		if op != "+" && op != "*" {
+			log.Fatalf("invalid operator: %q", op)
+		}
+
+		nums := []int64{}
+		for col := start; col < end; col++ {
+			var sb strings.Builder
+			for r := 0; r < rows-1; r++ {
+				ch := grid[r][col]
+				if ch != ' ' {
+					sb.WriteRune(ch)
+				}
+			}
+			if sb.Len() == 0 {
+				continue
+			}
+			n, err := strconv.ParseInt(sb.String(), 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			nums = append(nums, n)
+		}
+
+		if len(nums) == 0 {
+			continue
+		}
+
+		res := nums[0]
+		for i := 1; i < len(nums); i++ {
+			if op == "+" {
+				res += nums[i]
+			} else {
+				res *= nums[i]
+			}
+		}
+
+		total += res
+	}
+
+	fmt.Printf("total: %d\n", total)
+}
